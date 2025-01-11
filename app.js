0daa0640a845c6c4370a2d739d37408fe713ba43
Mithilesh-salunkhe <mithileshsalunkhe3@gmail.com>
2025-01-11 16:54:41 +0530

    undo the changes

--- app.js
+++ app.js
@@ -31,27 +31,17 @@ store.on("error" ,() =>{
     console.log("ERROR IN MONGO SESSION STORE" ,err);
 })
 
-// const sessionOptions = {
-//     store,
-//     secret : process.env.SECRET,
-//     resave : false,
-//     saveUninitialized:true,
-//     cookie:{
-//         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-//         maxAge : 7 * 24 * 60 * 60 * 1000,
-//         httpOnly : true,
-//     }
-// };
-
 const sessionOptions = {
-    secret: process.env.SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-      maxAge: 7 * 24 * 60 * 60 * 1000,
-    },
-  };
+    store,
+    secret : process.env.SECRET,
+    resave : false,
+    saveUninitialized:true,
+    cookie:{
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge : 7 * 24 * 60 * 60 * 1000,
+        httpOnly : true,
+    }
+};
 
 
 
