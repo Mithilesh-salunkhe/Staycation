5d6e24c5ecd7f6c7734b1e2314c9dc0cb0abe12e
Mithilesh-salunkhe <mithileshsalunkhe3@gmail.com>
2025-01-11 16:46:26 +0530

    changed session info in ap.js

--- app.js
+++ app.js
@@ -31,17 +31,27 @@ store.on("error" ,() =>{
     console.log("ERROR IN MONGO SESSION STORE" ,err);
 })
 
+// const sessionOptions = {
+//     store,
+//     secret : process.env.SECRET,
+//     resave : false,
+//     saveUninitialized:true,
+//     cookie:{
+//         expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+//         maxAge : 7 * 24 * 60 * 60 * 1000,
+//         httpOnly : true,
+//     }
+// };
+
 const sessionOptions = {
-    store,
-    secret : process.env.SECRET,
-    resave : false,
-    saveUninitialized:true,
-    cookie:{
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-        maxAge : 7 * 24 * 60 * 60 * 1000,
-        httpOnly : true,
-    }
-};
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    },
+  };
 
 
 
